30b3e4f6cb63d14c5f8681262ff81409c8a4db18
Simon Rozman <simon@rozman.si>
2020-12-29 12:56:07 +0100

    sign: Add description
    UAC prompt displays some random MSI filename otherwise.
    
    Signed-off-by: Simon Rozman <simon@rozman.si>

--- sign.bat
+++ sign.bat
@@ -3,13 +3,14 @@
 setlocal enabledelayedexpansion
 set BUILDDIR=%~dp0
 
-call :sign hr-apple || goto :error
-call :sign sl-apple || goto :error
+call :sign hr-apple "Apple Magic Keyboard Croatian" || goto :error
+call :sign sl-apple "Apple Magic Keyboard Slovenian" || goto :error
 exit /b 0
 
 :sign
 	set BASENAME=%1
-	signtool.exe sign /sha1 "%ManifestCertificateThumbprint%" /fd sha256 /tr "%ManifestTimestampRFC3161Url%" /td sha256 "%BUILDDIR%%BASENAME%\amd64\%BASENAME%.dll" "%BUILDDIR%%BASENAME%\i386\%BASENAME%.dll" "%BUILDDIR%%BASENAME%\ia64\%BASENAME%.dll" "%BUILDDIR%%BASENAME%\wow64\%BASENAME%.dll" "%BUILDDIR%%BASENAME%\%BASENAME%_amd64.msi" "%BUILDDIR%%BASENAME%\%BASENAME%_i386.msi" "%BUILDDIR%%BASENAME%\%BASENAME%_ia64.msi"
+	set DESCRIPTION=%2
+	signtool.exe sign /sha1 "%ManifestCertificateThumbprint%" /fd sha256 /tr "%ManifestTimestampRFC3161Url%" /td sha256 /d "%DESCRIPTION%" "%BUILDDIR%%BASENAME%\amd64\%BASENAME%.dll" "%BUILDDIR%%BASENAME%\i386\%BASENAME%.dll" "%BUILDDIR%%BASENAME%\ia64\%BASENAME%.dll" "%BUILDDIR%%BASENAME%\wow64\%BASENAME%.dll" "%BUILDDIR%%BASENAME%\%BASENAME%_amd64.msi" "%BUILDDIR%%BASENAME%\%BASENAME%_i386.msi" "%BUILDDIR%%BASENAME%\%BASENAME%_ia64.msi"
 	goto :eof
 
 :error
